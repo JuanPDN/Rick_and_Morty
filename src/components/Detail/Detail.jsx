ac2837378c6158e43b83cbc3158d02d80f305638
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-04-26 06:36:55 -0500

    Rick and Morty integration

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,14 +1,47 @@
 import '../Detail/detail.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 function Detail() {
-    return ( 
+
+    const { id } = useParams()
+    const [character, setCharacter] = useState({})
+
+    useEffect(() => {
+        axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+            if (data.name) {
+                setCharacter(data);
+            } else {
+                window.alert('No hay personajes con ese ID');
+            }
+        });
+        return setCharacter({});
+    }, [id]);
+
+
+    return (
         <div className="detail">
-            <h1>
-            Hola soy el Detail
-            </h1>
+            <div className='container'>
+
+                <div className='header'>
+                    {console.log(character)}
+                    <h1>
+                        {character.name}
+                    </h1>
+                    <img src={character.image} alt={character.name} />
+                </div>
+                <ul>
+                    <li>Status: {character.status}</li>
+                    <li>Gender: {character.gender}</li>
+                    <li>Specie: {character.species}</li>
+                    <li>Origin: {character.origin?.name}</li>
+                </ul>
+            </div>
+
         </div>
-     );
+    );
 }
 
 export default Detail;
