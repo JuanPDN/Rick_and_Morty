40ead1b8cf64b21a9633ffa745e07bacbc82ce01
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-04-28 19:51:28 -0500

    Ryck y Morty integration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
+import store from './redux/store'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root')
 
 createRoot(container).render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
