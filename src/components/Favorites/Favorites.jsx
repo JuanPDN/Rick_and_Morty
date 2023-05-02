89ee2e27de272c2bd19279ac56cc46bb7bba47c3
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-02 17:10:47 -0500

    Rick y Morty integration

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,32 +1,58 @@
 import { useDispatch, useSelector } from "react-redux";
 import Card from "../Card/Card";
-import { removeFav } from "../../redux/action";
+import { removeFav, filterCards, orderCards } from "../../redux/action";
+import { useState } from "react";
+import './favorites.css'
+
 export default function Favorites() {
 
-    const myFavorites = useSelector((state)=> state.myFavorites)
+    const [aux, setAux] = useState(false)
+    const myFavorites = useSelector((state) => state.myFavorites)
     const dispatch = useDispatch()
 
 
-        const remove = (id) => {
-            dispatch(removeFav(id))
-        }
+    const remove = (id) => {
+        dispatch(removeFav(id))
+    }
 
+    const handleOrder = (event) => {
+        dispatch(orderCards(event.target.value))
+        setAux(!aux)
+    }
+
+    const handleFilter = (event) => {
+        dispatch(filterCards(event.target.value))
+    }
 
     return (
-        <div className="container-cards">
-            {myFavorites.map((character) =>
-                <Card
-                    id={character.id}
-                    key={character.id}
-                    name={character.name}
-                    status={character.status}
-                    species={character.species}
-                    gender={character.gender}
-                    origin={character.origin.name}
-                    image={character.image}
-                    onClose={remove}
-                />
-            )}
+        <div>
+            <div className="fillters">
+                <select onChange={handleOrder}>
+                    <option value="A">Ascendente</option>
+                    <option value="D">Descendente</option>
+                </select>
+                <select onChange={handleFilter}>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Genderless">Genderless</option>
+                    <option value="unknow">unknow</option>
+                </select>
+            </div >
+            <div className="container-cards">
+                {myFavorites.map((character) =>
+                    <Card
+                        id={character.id}
+                        key={character.id}
+                        name={character.name}
+                        status={character.status}
+                        species={character.species}
+                        gender={character.gender}
+                        origin={character.origin}
+                        image={character.image}
+                        onClose={remove}
+                    />
+                )}
+            </div>
         </div>
     );
 }
