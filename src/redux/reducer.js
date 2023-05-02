0750c20e42385030f7c67bfc02776b96d873f498
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-02 07:09:52 -0500

    Rick y Morty integration

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-types";
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./action-types";
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 }
 
 export default function rootReducer(state = initialState, { type, payload }) {
@@ -9,14 +10,36 @@ export default function rootReducer(state = initialState, { type, payload }) {
         case ADD_FAV:
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, payload]
+                myFavorites: [...state.myFavorites, payload],
+                allCharacters: [...state.myFavorites, payload]
             }
 
         case REMOVE_FAV:
             return {
                 ...state,
-                myFavorites: [...state.myFavorites.filter(((fav) => fav.id !== Number(payload)))]
+                myFavorites: [...state.allCharacters.filter(((fav) => fav.id !== Number(payload)))],
+                allCharacters: [...state.allCharacters.filter(((fav) => fav.id !== Number(payload)))]
             }
+        case FILTER:
+            return {
+                ...state,
+                myFavorites: [...state.allCharacters.filter((fav) => fav.gender === payload)],
+            }
+
+        case ORDER:
+            return {
+                ...state,
+                myFavorites: state.allCharacters.sort((a, b) => {
+                    if (a.id > b.id){
+                        return 'A' === payload ? 1 : -1 
+                    }
+                    if (a.id < b.id){
+                        return 'D' === payload ? 1 : -1 
+                    }
+                    return 0
+                })
+            }
+
 
         default:
             return { ...state }
