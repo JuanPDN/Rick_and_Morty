65f5e6ecc08507002ad7f4f3f15dc6a4347aaa1a
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-10 18:36:09 -0500

    Rick and Morty integration

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,7 +1,28 @@
-const  {server}  = require('./index')
+const express = require ('express')
+const server = express()
+const router = require('./routes/index')
+const cors = require('cors')
 
-const PORT = 3001
 
-server.listen(PORT, () => {
-    console.log(`Server raised in port: ${PORT}`);
-})
+server.use(cors())
+server.use(express.json())
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+
+
+server.use('/rickandmorty', router)
+
+module.exports = {
+    server
+}
---
+++ Server/test/index.test.js
@@ -0,0 +1,12 @@
+const { server } = require('../src/app');
+const session = require('supertest');
+const agent = session(server);
+
+
+describe('Test de RUTAS', () => {
+    describe('GET /rickandmorty/character/:id', () => {
+        it('Responde con status: 200', async() => {
+            await agent.get('/rickandmorty/character/1').expect(200); 
+        })
+    })
+})
