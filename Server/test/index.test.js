0bc4d53ecba9516349e814fb79c37496ad9244e5
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-11 04:27:59 -0500

    Rick and Morty integration

--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -2,11 +2,10 @@ const { server } = require('../src/app');
 const session = require('supertest');
 const agent = session(server);
 
-
 describe('Test de RUTAS', () => {
     describe('GET /rickandmorty/character/:id', () => {
         it('Responde con status: 200', async () => {
-            await agent.get('/rickandmorty/character/1').expect(200);
+            await agent.get('/rickandmorty/character/1').expect(200)
         })
         it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
             const response = await agent.get('/rickandmorty/character/1')
