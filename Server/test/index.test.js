7673515daf46c938b3f9a6d72f114722680470e6
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-11 06:33:04 -0500

    Rick and Morty integration

--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -18,23 +18,35 @@ describe('Test de RUTAS', () => {
             expect(response).toHaveProperty(['body', 'origin'])
             expect(response).toHaveProperty(['body', 'image'])
         })
-        it('Si hay un error responde con status: 500',  async () => {
+        it('Si hay un error responde con status: 500', async () => {
             await agent.get('/rickandmorty/character/9999').expect(500)
         })
     })
 
-    describe('GET /rickandmorty/login',()=>{
-        it('Debe retornar true si el password y el email son correctos',async()=>{
+    describe('GET /rickandmorty/login', () => {
+        it('Debe retornar true si el password y el email son correctos', async () => {
             const response = await agent.get('/rickandmorty/login/?email=juan%40juan.com&password=juan123')
-            expect(response).toHaveProperty(['body','access'],true)
+            expect(response).toHaveProperty(['body', 'access'], true)
         })
-        it('Debe retornar false si el password es incorrecto',async()=>{
+        it('Debe retornar false si el password es incorrecto', async () => {
             const response = await agent.get('/rickandmorty/login/?email=juan%40juan.com&password=juan124')
-            expect(response).toHaveProperty(['body','access'],false)
+            expect(response).toHaveProperty(['body', 'access'], false)
         })
-        it('Debe retornar false si el email es incorrecto',async()=>{
+        it('Debe retornar false si el email es incorrecto', async () => {
             const response = await agent.get('/rickandmorty/login/?email=juan%40jua.com&password=juan123')
-            expect(response).toHaveProperty(['body','access'],false)
+            expect(response).toHaveProperty(['body', 'access'], false)
+        })
+    })
+
+    describe('POST /rickandmorty/fav', () => {
+        it('lo que se envia por body deber ser devuelto en un array', async () => {
+            const response = await agent.post('/rickandmorty/fav').send({ arg1: 3, arg2: 7 })
+            expect(response.body).toEqual([{ arg1: 3, arg2: 7 }])
+        })
+
+        it('Si  se vuelve a enviar un nuevo elemento por body, este debe ser devuelto en un arreglo que incluye un elemento enviado previamente.', async () => {
+            const response = await agent.post('/rickandmorty/fav').send({ arg1: 1, arg2: 2 })
+            expect(response.body).toEqual([{ arg1: 3, arg2: 7 }, { arg1: 1, arg2: 2 }])
         })
     })
 
