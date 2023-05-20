617e314eb7500dd4e0d551d953cb3738bc1d97e3
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-20 12:10:44 -0500

    Rick and Morty integration

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const FavoriteModel = require('./models/Favorite.js')
+const UserModel = require('./models/User.js')
 
 // EJERCICIO 03
 // A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
 // Recuerda pasarle la información de tu archivo '.env'.
-
 
 const URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`
 const sequelize = new Sequelize(
@@ -16,16 +17,18 @@ const sequelize = new Sequelize(
 // EJERCICIO 05
 // Debajo de este comentario puedes ejecutar la función de los modelos.
 
-//
+FavoriteModel(sequelize)
 
-//
+UserModel(sequelize)
 
 // Ejercicio 06
 // ¡Relaciona tus modelos aquí abajo!
-// const { User, Favorite } = sequelize.models;
+const { User, Favorite } = sequelize.models;
+User.belongsToMany(Favorite, {through : 'user_favorite'})
+Favorite.belongsToMany (User, {through : 'user_favorite'})
 
 module.exports = {
-   // User,
-   // Favorite,
+   User,
+   Favorite,
    conn: sequelize,
 };
