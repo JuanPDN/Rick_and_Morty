c269ea7c0b043ee01a91c80cf371a7b29f082226
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-21 14:42:38 -0500

    Rick and Morty integration Final

---
+++ Server/src/controllers/login.js
@@ -0,0 +1,26 @@
+const { User } = require('../DB_connection')
+
+
+const login = async(req, res) =>{
+    const {email, password} = req.query
+try {
+    if(!email || !password){
+        res.status(400).send('Faltan datos')
+    }
+    const user = await User.findOne({
+        where:{
+            email 
+        }
+    }) 
+    if(!user){
+        res.status(404).send('Usuario no encontrado')
+    }
+    return user.password === password ? res.status(200).json({access: true}):
+    res.status(403).send('Contraseña incorrecta')
+} catch (error) {
+    res.status(500).json({error: error.message})
+}
+
+}
+
+module.exports = login
--- Server/src/controllers/postUser.js
+++ Server/src/controllers/postUser.js
@@ -1,8 +1,23 @@
-const User = require('../DB_connection')
+const { User } = require('../DB_connection')
 
-const postUser = async(req, res) => {
-    const { email, password} = req.body  
- }
+const postUser = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        if (!email || !password) {
+            res.status(400).send('Faltan datos')
+        }
+        const user = await User.findOrCreate({
+            where: {
+                email,
+                password
+            }
+        })
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+
+}
 
 
 module.exports = postUser
