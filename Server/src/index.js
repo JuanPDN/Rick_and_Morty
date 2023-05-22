4f57d6a5030a7b981eb4459a646610617c30e785
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-22 06:04:41 -0500

    Deploy

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,8 +3,8 @@ const { conn } = require('./DB_connection')
 
 const PORT = 3001
 
-conn.sync({ force: true }).then(() => {
+// conn.sync({ force: true }).then(() => {
     server.listen(PORT, () => {
         console.log(`Server raised in port: ${PORT}`);
     })
-})
+//})
